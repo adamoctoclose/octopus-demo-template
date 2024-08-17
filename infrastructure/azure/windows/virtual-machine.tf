f409943579eab724ca93412e2251341c9796d479
adamoctoclose <54795433+adamoctoclose@users.noreply.github.com>
2024-08-17 16:42:58 +0100

    Update virtual-machine.tf

--- infrastructure/azure/windows/virtual-machine.tf
+++ infrastructure/azure/windows/virtual-machine.tf
@@ -83,6 +83,7 @@ resource "azurerm_network_interface" "nic" {
 # Create a Windows virtual machine
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "demo-windows"
+  computer_name       = "demo-windows"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   size                = var.vm_size
